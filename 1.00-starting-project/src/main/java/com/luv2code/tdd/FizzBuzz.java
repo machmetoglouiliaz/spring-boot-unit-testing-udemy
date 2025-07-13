d807387dfd237c6af795d41ee13052a955ec1412
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-07-13 20:54:06 +0300

    refactor: change in implementation of fizzbuzz compute to be more efficient

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -3,9 +3,10 @@ package com.luv2code.tdd;
 public class FizzBuzz {
 
     public static String compute(int number){
-        if(number % 15 == 0) return "FizzBuzz";
-        if(number % 3 == 0) return "Fizz";
-        if(number % 5 == 0) return "Buzz";
-        return Integer.toString(number);
+        StringBuilder result = new StringBuilder();
+        if(number % 3 == 0) result.append("Fizz");
+        if(number % 5 == 0) result.append("Buzz");
+        if(result.isEmpty()) result.append(number);
+        return result.toString();
     }
 }
