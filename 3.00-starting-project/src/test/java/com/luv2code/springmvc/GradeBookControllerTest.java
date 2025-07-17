161e61ec8fe33231cc2ea5d15bc76fe2594c8bd3
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-07-17 23:40:45 +0300

    test(web): initial setup for Spring MVC controller test using MockMvc
    - Configured initial test class for Spring MVC web controller
    - Integrated MockMvc to simulate HTTP requests
    - Added assertion to verify returned view name

---
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
@@ -0,0 +1,79 @@
+package com.luv2code.springmvc;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource("/application.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class GradeBookControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private StudentAndGradeService studentService;
+
+    @BeforeEach
+    public void beforeEach() {
+
+        jdbc.execute("INSERT INTO student(firstname, lastname, email_address) " + "VALUES ('Mourat', 'Achmet', 'm@g.com')");
+    }
+
+    @AfterEach
+    public void afterEach() {
+
+        jdbc.execute("DELETE FROM student");
+
+        jdbc.execute("ALTER TABLE student ALTER COLUMN ID RESTART WITH 1");
+    }
+
+    @Test
+    public void getStudentsHttpRequest() throws Exception {
+
+        CollegeStudent student1 = new CollegeStudent("Mourat", "Achmet", "m@g.com");
+        CollegeStudent student2 = new CollegeStudent("Mourat", "Achmet2", "m2@g.com");
+
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(student1, student2));
+
+        when(studentService.getGradebook()).thenReturn(collegeStudentList);
+
+        assertIterableEquals(collegeStudentList, studentService.getGradebook());
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
+
+
+}
