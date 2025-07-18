556f119d12adcddf07c59169581d0540bca4d0c9
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-07-18 22:39:01 +0300

    test(web): initialize TDD setup for MVC controller with MockMvc

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
@@ -3,18 +3,20 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,12 +26,15 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @TestPropertySource("/application.properties")
 @AutoConfigureMockMvc
 @SpringBootTest
 public class GradeBookControllerTest {
+
+    private static MockHttpServletRequest request;
 
     @Autowired
     private JdbcTemplate jdbc;
@@ -39,6 +44,14 @@ public class GradeBookControllerTest {
 
     @Mock
     private StudentAndGradeService studentService;
+
+    @BeforeAll
+    public static void setup(){
+        request = new MockHttpServletRequest();
+        request.addParameter("firstname", "Mourat");
+        request.addParameter("lastname", "Achmet");
+        request.addParameter("email", "m@g.com");
+    }
 
     @BeforeEach
     public void beforeEach() {
@@ -67,6 +80,21 @@ public class GradeBookControllerTest {
         assertIterableEquals(collegeStudentList, studentService.getGradebook());
 
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
+
+    @Test
+    public void createStudentHttpRequest() throws Exception{
+        MvcResult mvcResult = this.mockMvc.perform(post("/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("firstname", request.getParameterValues("firstname"))
+                .param("lastname", request.getParameterValues("lastname"))
+                .param("email", request.getParameterValues("email")))
                 .andExpect(status().isOk())
                 .andReturn();
 
